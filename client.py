2365f4e00eb0696e4c4cac443e571a0aa0fc2e5d
MuhdHarithHazim <106472376+MuhdHarithHazim@users.noreply.github.com>
2022-07-16 20:14:53 +0800

    Update client.py

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import socket
 host = '192.168.56.104'
 port = 8989
 
+
 ClientSocket = socket.socket()
 print('Waiting for connection')
 try:
@@ -10,6 +11,8 @@ try:
 except socket.error as e:
     print(str(e))
 Response = ClientSocket.recv(2048)
+
+
 
 while True:
     currency = input('Convert To Currency (dollar,pound,euro,riyal): ')
@@ -19,8 +22,8 @@ while True:
     ClientSocket.send(str.encode(ammount))
     
     if currency == 'none':
-            break
+    break
     
     Response = ClientSocket.recv(2048)
     print(Response.decode('utf-8'))
-ClientSocket.close()
+    ClientSocket.close()
